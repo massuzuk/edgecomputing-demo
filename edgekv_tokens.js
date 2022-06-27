dd0a4f4fad6e0410e444bba6731749d96e856f95
massuzuk <massuzuk0629@gmail.com>
2022-06-27 11:04:56 +0900

    Delete edgekv_tokens.js

--- edgekv_tokens.js
+++
@@ -1,8 +0,0 @@
-var edgekv_access_tokens = {
-        "namespace-msuzuki": {
-            "name": "msuzuki_token1",
-	    "value": "input_your_jwt_token"
-
-        }
-}
-export { edgekv_access_tokens };
