150e530995c77195cd66dde1f869b1fbba35d021
massuzuk <massuzuk0629@gmail.com>
2022-06-27 11:04:19 +0900

    Add files via upload

---
+++ caching-on-demand/edgekv_tokens.js
@@ -0,0 +1,8 @@
+var edgekv_access_tokens = {
+        "namespace-msuzuki": {
+            "name": "msuzuki_token1",
+	    "value": "input_your_jwt_token"
+
+        }
+}
+export { edgekv_access_tokens };
